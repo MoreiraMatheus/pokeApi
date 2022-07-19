5d71810bf75ce5cfa553bc862b8389aef782316f
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2022-07-18 23:34:21 -0300

    deletando função não necessária

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,33 @@
 const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/'
 const BT_BUSCA_POKE = document.querySelector('button')
+const RESULT = document.getElementById('resultado')
 
 BT_BUSCA_POKE.addEventListener('click', () => {
     const ID_POKE = document.querySelector('input').value
-    !ID_POKE ? window.alert('informe o id do pokemon') : console.log(`valor: ${ID_POKE}`)
-    tratamentoDadosPoke(consultaDadosPoke(ID_POKE))
+    if(!ID_POKE){
+        window.alert('informe o id do pokemon')
+    }
+    else{
+        const DADOS_DA_API = consultaDadosPoke(ID_POKE) 
+        DADOS_DA_API.then(res => {
+            RESULT.innerText += res.name + '\n'
+        })
+        DADOS_DA_API.then(res => {
+            RESULT.innerText += res.id + '\n'
+        })
+        DADOS_DA_API.then(res => {
+            const COMPRIMENTO = res.types.length
+            // let tiposPoke = []
+            for(let i = 0; i <= COMPRIMENTO -1 ; i++){
+                RESULT.innerText += res.types[i].type.name + '\n'
+            }
+        })
+    }
 })
 
+// faz requisição direto da API
 async function consultaDadosPoke(id){
-    if(!id){
-        console.log('id não informado')
-    }
-    const DADOS = await fetch(BASE_URL + id + '/')
-    const DADOS_JSON = await DADOS.json()
+    const URL_COMPLETA = await fetch(BASE_URL + id + '/')
+    const DADOS_JSON = await URL_COMPLETA.json()
     return await DADOS_JSON
-}
-
-async function tratamentoDadosPoke(promessa){
-   promessa.then(res => {
-    console.log(res.name)
-    console.log(res.id)
-    // console.log(res.types[1].type.name)
-    for(let i = 0; i <= res.types.length; i++){
-        console.log(res.types[i].type.name)
-    }
-   })
 }
