d110983c3bb97f620140cf5144b701c69391f5d6
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2022-07-21 11:36:02 -0300

    tentando trocar para a versão shiny

--- src/js/script.js
+++ src/js/script.js
@@ -1,43 +1,39 @@
 const URL_DADOS = 'https://pokeapi.co/api/v2/pokemon/'
 const BT_BUSCA_POKE = document.querySelector('button')
-const BT_SHINY = document.getElementById('bt-shiny')
 const POKEDEX = document.getElementById('pokedex')
 const NOME_ID = document.getElementById('nome-e-id')
-const MOSTRA_INFOS = document.getElementById('image-poke')
+const IMAGE_POKE = document.getElementById('image-poke')
 const TIPOS = document.getElementById('tipos-do-poke')
 let isShiny = false
 
 BT_BUSCA_POKE.addEventListener('click', () => {
     const ID_POKE = document.querySelector('input').value
-    if(ID_POKE == '' || ID_POKE <= 0){
-        window.alert('informe um id válido')
+    if(!ID_POKE || Number(ID_POKE) <= 0){
+        window.alert('Digite valores válidos para busca')
     }
     else{
-        NOME_ID.innerHTML = ''
-        MOSTRA_INFOS.innerHTML = '<button id="bt-shiny">Shiny</button>'
-        TIPOS.innerHTML = ''
         const DADOS_DO_POKEMON = consultaApiPoke(ID_POKE)
-        
         DADOS_DO_POKEMON.then(res => {
+            reset()
             NOME_ID.appendChild(criaHtml('span', String(res.name).toLocaleUpperCase()))
             NOME_ID.appendChild(criaHtml('span', '#' + tratamentoID(res.id)))
-
+            
             const IMG_NAO_ENCONTRADA = '<img src="src/img/ponto-de-interrogacao.png" alt="foto não encontrada">'
-            const IMG = res.sprites.front_default
-            // const IMG_SHINY = res.sprites.front_shiny
-            !IMG ? MOSTRA_INFOS.innerHTML += IMG_NAO_ENCONTRADA : MOSTRA_INFOS.innerHTML += `<img src="${IMG}" alt="imagem-pokemon">`
-            // !IMG_SHINY ? MOSTRA_INFOS.innerHTML += IMG_NAO_ENCONTRADA : MOSTRA_INFOS.innerHTML += `<img src="${IMG_SHINY}" alt="foto não encontrada">`
+            const IMG_NORMAL = res.sprites.front_default
+            const IMG_SHINY = res.sprites.front_shiny
+            //arrumar uma forma de trocar da foto normal pra foto shiny
+            !IMG_NORMAL || !IMG_SHINY ? IMAGE_POKE.innerHTML += IMG_NAO_ENCONTRADA : IMAGE_POKE.innerHTML += `<img src="${IMG_NORMAL}" alt="imagem-pokemon">`
             
             const COMPRIMENTO = res.types.length
             for(let i = 0; i <= COMPRIMENTO -1 ; i++){
                 TIPOS.appendChild(criaHtml('p', res.types[i].type.name))
             }
         })
-    }
-})
 
-BT_SHINY.addEventListener('click', () => {
-    window.alert('ai ai ')
+        DADOS_DO_POKEMON.catch(() => {
+            window.alert('pokemon não encontrado')
+        })
+    }
 })
 
 async function consultaApiPoke(id){
@@ -65,4 +61,32 @@ function tratamentoID(id){
     else{
         return '00' + ID
     }
+}
+
+//função que talvez não seja usada
+function mostraShiny(normal=null, shiny=null){
+    if(normal == null || shiny == null){
+        window.alert('em desenvolvimento')
+    }
+    else {
+        if(isShiny){
+            window.alert('vamos ver sua versão shiny')
+            IMAGE_POKE.innerHTML += `<img src="${shiny}" alt="imagem-pokemon">`
+        }
+        else{
+            window.alert('vamos ver sua versão normal')
+            IMAGE_POKE.innerHTML += `<img src="${normal}" alt="imagem-pokemon">`
+        }
+        isShiny ? isShiny = false : isShiny = true
+    }
+}
+
+function reset(){
+    NOME_ID.innerHTML = ''
+    IMAGE_POKE.innerHTML = ''
+    const BT_PARA_SHINY = document.createElement('button')
+    BT_PARA_SHINY.innerText = 'Shiny'
+    BT_PARA_SHINY.setAttribute('onclick', `mostraShiny()`)
+    IMAGE_POKE.appendChild(BT_PARA_SHINY)
+    TIPOS.innerHTML = ''
 }
